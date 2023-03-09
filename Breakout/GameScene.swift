70164c54560a5e70eef40408e3269d4819fee287
Dhanush Tipparaju <dhanushtips@gmail.com>
2023-03-09 09:45:51 -0600

    created dynamic ball behavior

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -8,16 +8,19 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     var ball = SKShapeNode()
     var paddle = SKSpriteNode()
     var brick = SKSpriteNode()
     var loseZone = SKSpriteNode()
     override func didMove(to view: SKView) {
         //this stuff happens once (when the app open)
+        physicsWorld.contactDelegate = self
+        self.physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
         createBackground()
         resetGame()
         makeLoseZone()
+        kickBall()
     }
     
     func resetGame(){
@@ -25,6 +28,11 @@ class GameScene: SKScene {
         makeBall()
         makePaddle()
         makeBrick()
+    }
+    
+    func kickBall() {
+        ball.physicsBody?.isDynamic = true
+        ball.physicsBody?.applyImpulse(CGVector(dx: 3, dy: 5))
     }
     
     func createBackground() {
